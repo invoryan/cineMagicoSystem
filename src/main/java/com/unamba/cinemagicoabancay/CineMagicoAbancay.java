35642479c95b3a860b3e9f0401499ca087a97fbf
Bryan Hurtado Delgado <bryanhurtadodelgado@gmail.com>
2022-12-26 08:20:22 -0500

    imprime hola mundo

--- src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
+++ src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
@@ -2,6 +2,6 @@ package com.unamba.cinemagicoabancay;
 public class CineMagicoAbancay {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundos");
     }
 }
