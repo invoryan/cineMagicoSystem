35688b981611a19d7d1ce72f738e0813a2782f10
Bryan Hurtado Delgado <bryanhurtadodelgado@gmail.com>
2022-12-26 09:23:04 -0500

    imprime simulacion con nueva rama

--- src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
+++ src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
@@ -1,8 +1,6 @@
 package com.unamba.cinemagicoabancay;
 public class CineMagicoAbancay {
-//hola
-//mundo
     public static void main(String[] args) {
-        System.out.println("simulacion");
+        System.out.println("simulacion2");
     }
 }
