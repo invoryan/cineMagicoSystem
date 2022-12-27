06d285a245d07d82e8104e705db367a9b05808d5
Bryan Hurtado Delgado <bryanhurtadodelgado@gmail.com>
2022-12-27 15:59:53 -0500

    imprime simulacion67

--- src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
+++ src/main/java/com/unamba/cinemagicoabancay/CineMagicoAbancay.java
@@ -1,6 +1,6 @@
 package com.unamba.cinemagicoabancay;
 public class CineMagicoAbancay {
     public static void main(String[] args) {
-        System.out.println("simulacion3");
+        System.out.println("simulacion67");
     }
 }
